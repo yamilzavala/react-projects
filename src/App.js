7a375b199cd050d5c877780e2ce00a451c7ce267
yamilzavala <zavalayamil9@gmail.com>
2024-04-01 17:23:08 -0300

    remove project comfy-store

--- src/App.js
+++ src/App.js
@@ -21,7 +21,6 @@ import AppReactQuery from './projects/06-react-query/starter/AppReactQuery';
 import AppUnplashImages from './projects/07-unsplash-images/starter/AppUnplashImages';
 import AppReactQueryReactRouter from './projects/09-mixmaster/starter/AppReactQueryReactRouter';
 import AppReduxToolkit from './projects/11-redux-toolkit-tutorial/starter/AppReduxToolkit';
-import AppConfyStore from './projects/12-comfy-store/AppConfyStore';
 
 
 function App() {
@@ -59,10 +58,7 @@ function App() {
       {/* <AppReactQueryReactRouter/> */}
 
       {/* Redux tolkit */}
-      {/* <AppReduxToolkit/> */}
-
-      {/* React router - rtk - axios - React query */}
-      <AppConfyStore/>
+      <AppReduxToolkit/>
       
     </div>
   );
--- src/projects/12-comfy-store/components/index.js
+++
@@ -1,4 +0,0 @@
-export {default as FormInput} from "./FormInput";
-export {default as SubmitBtn} from "./SubmitBtn";
-export {default as LoginForm} from "./LoginForm";
-export {default as RegisterForm} from "./RegisterForm";
